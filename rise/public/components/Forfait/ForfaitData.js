8169b009fef4825a7675a84ea1d88bb098635102
yassineoo <jy_attou@esi.dz>
2025-09-10 15:31:43 +0100

    fix cards

--- rise/public/components/Forfait/ForfaitData.js
+++ rise/public/components/Forfait/ForfaitData.js
@@ -6,105 +6,105 @@ export default {
         data: "50Go",
         price: "1500",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "ooredoo internet 2500",
         data: "120Go",
         price: "2500",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "ooredoo internet 4500",
         data: "200Go",
         price: "4500",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "ooredoo internet 5500",
         data: "250Go",
         price: "5500",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "ooredoo internet 10000",
         data: "500Go",
         price: "10000",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "ooredoo internet 19500",
         data: "1000Go",
-        price: "19500",
+        price: "19 500",
         duration: "4 Semaines",
-        buy: "Acheter"
-      }
+        buy: "Acheter",
+      },
     ],
     internetForfaits: [
       {
-        topLabel: "600Go par AN",
+        topLabel: "600Go PAR AN",
         name: "15 000 DA",
-        sub:"1500",
+        sub: "1500",
         data: "50Go x 12",
-        price: "15000",
+        price: "15 000",
         oldPrice: "18 000",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
-        topLabel: "1440Go par AN",
+        topLabel: "1440Go PAR AN",
         name: "25 000 DA",
-        sub:"2500",
+        sub: "2500",
         data: "12Go x 12",
-        price: "25000",
+        price: "25 000",
         oldPrice: "30 000",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
-        topLabel: "2400Go par AN",
+        topLabel: "2400Go PAR AN",
         name: "45 000 DA",
-        sub:"4500",
+        sub: "4500",
         data: "200Go x 12",
-        price: "45000",
+        price: "45 000",
         oldPrice: "54 000",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
-        topLabel: "3000Go par AN",
+        topLabel: "3000Go PAR AN",
         name: "55 000 DA",
-        sub:"5500",
+        sub: "5500",
         data: "250Go x 12",
-        price: "55000",
-        oldPrice:"66 000",
+        price: "55 000",
+        oldPrice: "66 000",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
-        topLabel: "6000Go par AN",
+        topLabel: "6000Go PAR AN",
         name: "100 000 DA",
-        sub:"10000",
+        sub: "10000",
         data: "500Go x 12",
-        price: "100000",
+        price: "100 000",
         oldPrice: "120 000",
         duration: "4 Semaines",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
-        topLabel: "12000Go par AN",
+        topLabel: "12000Go PAR AN",
         name: "195 000 DA",
-        sub:"19500",
+        sub: "19500",
         data: "1000Go x 12",
-        price: "195000",
-        oldPrice:"234 000",
+        price: "19 5000",
+        oldPrice: "234 000",
         duration: "4 Semaines",
-        buy: "Acheter"
-      }
+        buy: "Acheter",
+      },
     ],
     smartForfaits: [
       {
@@ -113,18 +113,15 @@ export default {
         features: ["600 DA de crédit"],
         price: "200 ",
         duration: "24 heures",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "Smart 100",
         data: "500Mo internet",
-        features: [
-          "Facebook et Messenger illimités",
-          "100 DA de crédit"
-        ],
+        features: ["Facebook et Messenger illimités", "100 DA de crédit"],
         price: "100 ",
         duration: "24 heures",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "Smart 50",
@@ -132,199 +129,196 @@ export default {
         features: [],
         price: "50 ",
         duration: "24 heures",
-        buy: "Acheter"
-      }
+        buy: "Acheter",
+      },
     ],
     hadraForfaits: [
       {
         name: "200 DA",
         data: "Appels illimités vers Ooredoo",
         price: "200",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "300 DA",
         data: "600 DA de bonus",
         price: "300",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "1000 DA",
         data: "Appels illimités vers tous les réseaux",
         features: [],
         price: "1000",
-        buy: "Acheter"
-      }
+        buy: "Acheter",
+      },
     ],
     labels: {
       titleData: "FORFAITS INTERNET",
       titleSmart: "FORFAITS SMART",
-      buy: "Acheter"
-    }
+      buy: "Acheter",
+    },
   },
 
-ar: {
-  forfaits: [
-    {
-      name: "إنترنت 1500",
-      data: "50Go",
-      price: "1500",
-      duration: "4 أسابيع",
-      buy: "شراء"
-    },
-    {
-      name: "إنترنت 2500",
-      data: "120Go",
-      price: "2500",
-      duration: "4 أسابيع",
-      buy: "شراء"
-    },
-    {
-      name: "إنترنت 4500",
-      data: "200Go",
-      price: "4500",
-      duration: "4 أسابيع",
-      buy: "شراء"
-    },
-    {
-      name: "إنترنت 5500",
-      data: "250Go",
-      price: "5500",
-      duration: "4 أسابيع",
-      buy: "شراء"
-    },
-    
-    {
-      name: "إنترنت 10000",
-      data: "500Go",
-      price: "10000",
-      duration: "4 أسابيع",
-      buy: "شراء"
-    },
-    {
-      name: "إنترنت 19500",
-      data: "1000Go",
-      price: "19500",
-      duration: "4 أسابيع",
-      buy: "شراء"
-    }
-  ],
-  internetForfaits : [
-  {
-    topLabel: "600 جيغا للسنة",
-    name: "000 15 دج",
-    sub:"1500",
-    data: "50 جيغا × 12",
-    price: "000 15",
-    oldPrice: "000 18",
-    duration: "4 أسابيع",
-    buy: "اشترِ"
-  },
-  {
-    topLabel: "1440 جيغا للسنة",
-    name: "000 025 دج",
-    sub:"2500",
-    data: "120 جيغا × 12",
-    price: "000 25",
-    oldPrice: "000 30",
-    duration: "4 أسابيع",
-    buy: "اشترِ"
-  },
-  {
-    topLabel: "2400 جيغا للسنة",
-    name: "000 045 دج",
-    sub:"4500",
-    data: "200 جيغا × 12",
-    price: "000 45",
-    oldPrice: "000 54",
-    duration: "4 أسابيع",
-    buy: "اشترِ"
-  },
-  {
-    topLabel: "3000 جيغا للسنة",
-    name: "000 55 دج",
-    sub:"5500",
-    data: "250 جيغا × 12",
-    price: "000 55",
-    oldPrice: "000 66",
-    duration: "4 أسابيع",
-    buy: "اشترِ"
-  },
-  {
-    topLabel: "6000 جيغا للسنة",
-    name: "000 100 دج",
-    sub:"10000",
-    data: "500 جيغا × 12",
-    price: "000 100",
-    oldPrice: "000 120",
-    duration: "4 أسابيع",
-    buy: "اشترِ"
-  },
-  {
-    topLabel: "12000 جيغا للسنة",
-    name: "000 195 دج",
-    sub:"19500",
-    data: "1000 جيغا × 12",
-    price: "000 195",
-    oldPrice: "000 234",
-    duration: "4 أسابيع",
-    buy: "اشترِ"
-  }
-],
-hadraForfaits: [
+  ar: {
+    forfaits: [
+      {
+        name: "إنترنت 1500",
+        data: "50Go",
+        price: "1500",
+        duration: "4 أسابيع",
+        buy: "شراء",
+      },
+      {
+        name: "إنترنت 2500",
+        data: "120Go",
+        price: "2500",
+        duration: "4 أسابيع",
+        buy: "شراء",
+      },
+      {
+        name: "إنترنت 4500",
+        data: "200Go",
+        price: "4500",
+        duration: "4 أسابيع",
+        buy: "شراء",
+      },
+      {
+        name: "إنترنت 5500",
+        data: "250Go",
+        price: "5500",
+        duration: "4 أسابيع",
+        buy: "شراء",
+      },
+
+      {
+        name: "إنترنت 10000",
+        data: "500Go",
+        price: "10000",
+        duration: "4 أسابيع",
+        buy: "شراء",
+      },
+      {
+        name: "إنترنت 19500",
+        data: "1000Go",
+        price: "19500",
+        duration: "4 أسابيع",
+        buy: "شراء",
+      },
+    ],
+    internetForfaits: [
+      {
+        topLabel: "600 جيغا للسنة",
+        name: "000 15 دج",
+        sub: "1500",
+        data: "50 جيغا × 12",
+        price: "000 15",
+        oldPrice: "000 18",
+        duration: "4 أسابيع",
+        buy: "اشترِ",
+      },
+      {
+        topLabel: "1440 جيغا للسنة",
+        name: "000 025 دج",
+        sub: "2500",
+        data: "120 جيغا × 12",
+        price: "000 25",
+        oldPrice: "000 30",
+        duration: "4 أسابيع",
+        buy: "اشترِ",
+      },
+      {
+        topLabel: "2400 جيغا للسنة",
+        name: "000 045 دج",
+        sub: "4500",
+        data: "200 جيغا × 12",
+        price: "000 45",
+        oldPrice: "000 54",
+        duration: "4 أسابيع",
+        buy: "اشترِ",
+      },
+      {
+        topLabel: "3000 جيغا للسنة",
+        name: "000 55 دج",
+        sub: "5500",
+        data: "250 جيغا × 12",
+        price: "000 55",
+        oldPrice: "000 66",
+        duration: "4 أسابيع",
+        buy: "اشترِ",
+      },
+      {
+        topLabel: "6000 جيغا للسنة",
+        name: "000 100 دج",
+        sub: "10000",
+        data: "500 جيغا × 12",
+        price: "000 100",
+        oldPrice: "000 120",
+        duration: "4 أسابيع",
+        buy: "اشترِ",
+      },
+      {
+        topLabel: "12000 جيغا للسنة",
+        name: "000 195 دج",
+        sub: "19500",
+        data: "1000 جيغا × 12",
+        price: "000 195",
+        oldPrice: "000 234",
+        duration: "4 أسابيع",
+        buy: "اشترِ",
+      },
+    ],
+    hadraForfaits: [
       {
         name: "200 دج",
         data: "مكالمات غير محدودة نحو اوريدو",
         price: "200",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "300 دج",
         data: "600 دج رصيد مُهدى",
         price: "300",
-        buy: "Acheter"
+        buy: "Acheter",
       },
       {
         name: "1000 دج",
         data: "مكالمات غير محدودة نحو كل الشبكات",
         features: [],
         price: "1000",
-        buy: "Acheter"
-      }
+        buy: "Acheter",
+      },
     ],
-  smartForfaits: [
-    {
-      name: "Smart 200",
-      data: "إنترنت مجاني",
-      features: ["600 دج رصيد"],
-      price: "200 ",
-      duration: "24 ساعة",
-      buy: "شراء"
+    smartForfaits: [
+      {
+        name: "Smart 200",
+        data: "إنترنت مجاني",
+        features: ["600 دج رصيد"],
+        price: "200 ",
+        duration: "24 ساعة",
+        buy: "شراء",
+      },
+      {
+        name: "Smart 100",
+        data: "500Mo إنترنت",
+        features: ["فايسبوك & ماسنجر مجانيين", "100 دج رصيد"],
+        price: "100 ",
+        duration: "24 ساعة",
+        buy: "شراء",
+      },
+      {
+        name: "Smart 50",
+        data: "200Mo إنترنت",
+        features: [],
+        price: "50 ",
+        duration: "24 ساعة",
+        buy: "شراء",
+      },
+    ],
+    labels: {
+      titleData: "اشتراكات الإنترنت",
+      titleSmart: "SMART اشتراكات",
+      buy: "شراء",
     },
-    {
-      name: "Smart 100",
-      data: "500Mo إنترنت",
-      features: [
-        "فايسبوك & ماسنجر مجانيين",
-        "100 دج رصيد"
-      ],
-      price: "100 ",
-      duration: "24 ساعة",
-      buy: "شراء"
-    },
-    {
-      name: "Smart 50",
-      data: "200Mo إنترنت",
-      features: [],
-      price: "50 ",
-      duration: "24 ساعة",
-      buy: "شراء"
-    }
-  ],
-  labels: {
-    titleData: "اشتراكات الإنترنت",
-    titleSmart: "SMART اشتراكات",
-    buy: "شراء"
-  }
-}
+  },
 };
--- rise/public/components/Forfait/Slider.js
+++ rise/public/components/Forfait/Slider.js
@@ -74,14 +74,16 @@ export class Slider {
 
           <div class="flex-1 text-center border-b-[1px] border-b-[#BBBEBE] border-dashed">
             <div class="flex items-center justify-center">
-              <h3 class="${dataFontClass} py-7 px-5 text-4xl font-semibold text-ooredoo-red dark:text-white ${textAlign} leading-10">${offer.data}</h3>
+              <h3 class="${dataFontClass} py-7 px-5 text-4xl font-semibold text-ooredoo-red dark:text-white ${textAlign} leading-10">${
+      offer.data
+    }</h3>
             </div>
           </div>
 
           <div class="forfait-card-footer pt-4">
             <div class="flex justify-center items-baseline w-full mb-4">
               <div class="flex items-baseline justify-center" style="width:70%;">
-                <span class="${priceFontClass} font-bold mx-2 text-[27.96px] leading-none text-black dark:text-white">${priceNumber}</span>
+                <span class="${priceFontClass} font-semibold mx-2 text-[27.96px] leading-none text-black dark:text-white">${priceNumber}</span>
                 <span class="${priceFontClass} font-semibold text-base leading-none text-black dark:text-white whitespace-nowrap">${currencyLabel}</span>
                 <span class="${priceFontClass} font-semibold leading-none text-black dark:text-white whitespace-nowrap ${
       durationText.includes("cycle") ? "text-xs" : "text-base"
@@ -138,7 +140,7 @@ export class Slider {
         </div>
         <div class="h-full bg-white dark:bg-[#2C2C2C] pb-6 rounded-xl border-[1px] border-ooredoo-red dark:border-white"}>
           <div class="h-14 bg-ooredoo-red dark:border-ooredoo-red -m-[1px] border-2 flex items-center justify-center rounded-t-xl">
-            <h2 class="text-white font-semibold text-xl text-center capitalize dark:text-white leading-tight">
+            <h2 class="text-white font-bold text-[25px]  text-center capitalize dark:text-white leading-tight">
               ${offer.price} ${currencyLabel}
             </h2>
           </div>
@@ -150,24 +152,24 @@ export class Slider {
           </div>
 
           <div class="flex-1 py-4 text-center border-b-[1px] border-t-[1px] border-t-[#BBBEBE] border-b-[#BBBEBE] border-dashed">
-            <p>${this.currentLang === 'ar' ? "قوموا بشراء" : "Payez"}</p>
-            <p class="text-ooredoo-red text-xl font-bold">${this.currentLang === 'ar' ? "10 اشتراكات" : "10 forfaits"}</p>
-            <p class="font-bold text-xl">Ooredoo Internet ${offer.sub}</p>
+            <p>${this.currentLang === "ar" ? "قوموا بشراء" : "Payez"}</p>
+            <p class="text-ooredoo-red text-xl font-bold">${this.currentLang === "ar" ? "10 اشتراكات" : "10 forfaits"}</p>
+            <p class="font-semibold text-xl">Ooredoo Internet ${offer.sub}</p>
             <p>${isRTL ? "و" : "&"}</p>
-            <p>${this.currentLang === 'ar' ? "احصلوا على" : "Obtenez"}</p>
-            <p class="text-ooredoo-red font-bold text-xl">${this.currentLang === 'ar' ? "اشتراكين مجانا" : "2 gratuits"} !</p>
+            <p>${this.currentLang === "ar" ? "احصلوا على" : "Obtenez"}</p>
+            <p class="text-ooredoo-red font-bold text-xl">${this.currentLang === "ar" ? "اشتراكين مجانا" : "2 gratuits"} !</p>
           </div>
 
           <div class="p-4 flex items-center justify-between">
             <div class="text-center">
-              <p class="text-sm">${this.currentLang === 'ar' ? "احصلوا على" : "Recevez"}</p>
-              <p class="font-bold text-xl">${this.currentLang === 'ar' ? "12 اشتراك" : "12 forfaits"}</p>
+              <p class="text-sm">${this.currentLang === "ar" ? "احصلوا على" : "Recevez"}</p>
+              <p class="font-semibold text-xl">${this.currentLang === "ar" ? "12 اشتراك" : "12 forfaits"}</p>
             </div>
             <div class="text-center">
-              <p class="text-[#7F7F7F] font-bold text-xl line-through decoration-red-500">
+              <p class="text-[#7F7F7F] font-semibold text-xl line-through decoration-red-500">
                 ${offer.oldPrice} ${currencyLabel}
               </p>
-              <p class="font-bold text-3xl">${offer.price} ${currencyLabel}</p>
+              <p class="font-semibold text-3xl">${offer.price} ${currencyLabel}</p>
             </div>
           </div>
 
@@ -220,7 +222,9 @@ export class Slider {
     const priceFontClass = isRTL ? "font-noto-kufi-arabic" : "font-rubik";
 
     return `
-      <div class="${(index-12) === 2 && "md:col-span-2 md:justify-self-center lg:col-span-1 lg:justify-self-auto"}relative bg-white px-4 pb-6 dark:bg-[#2C2C2C] rounded-xl flex flex-col w-full mx-auto forfait-card-shadow overflow-hidden" style="max-width: 340px;">
+      <div class="${
+        index - 12 === 2 && "md:col-span-2 md:justify-self-center lg:col-span-1 lg:justify-self-auto"
+      }relative bg-white px-4 pb-6 dark:bg-[#2C2C2C] rounded-xl flex flex-col w-full mx-auto forfait-card-shadow overflow-hidden" style="max-width: 340px;">
         <div class="h-full flex flex-col justify-between" ${isRTL ? `dir="rtl"` : ``}>
           <div class="">
             <div class="border-b-[1px] border-b-[#BBBEBE] border-dashed text-center py-4">
@@ -312,12 +316,14 @@ export class Slider {
     const gridClass = gridType === "forfait-grid-5" ? "forfait-grid-5" : "forfait-grid-3";
     const sliderId = gridType === "forfait-grid-5" ? "forfaits-slider" : "internet-slider";
     const dotsId = gridType === "forfait-grid-5" ? "forfaits-dots" : "smart-dots";
-    const startIndex = 6
+    const startIndex = 6;
 
     return `
           <div class="hidden sm:flex w-full items-center justify-center">
             <div class="gap-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-              ${offers.map((offer, index) => this.createForfaitCardInternet(offer, startIndex + index, labels, isRTL, convertToLatinNumerals)).join("")}
+              ${offers
+                .map((offer, index) => this.createForfaitCardInternet(offer, startIndex + index, labels, isRTL, convertToLatinNumerals))
+                .join("")}
             </div>
           </div>
       
