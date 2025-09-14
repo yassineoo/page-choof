e40a202e95b1edfe244c49e74b4f066dfea9e23c
yassineoo <jy_attou@esi.dz>
2025-09-14 10:56:05 +0100

    checj

--- dima/public/components/DigitalServices/DigitalShahidServicesData.js
+++ dima/public/components/DigitalServices/DigitalShahidServicesData.js
@@ -3,53 +3,41 @@ export const shahidPlans = {
     {
       id: "shahid500",
       name: "SHAHID 500",
-      data: "02 Go internet",
-      features: [
-        "Accès shahid mobile",
-        "02 Go internet"
-      ],
+      data: "2Go internet",
+      features: ["Accès shahid mobile", "2Go internet"],
       price: "500",
-      duration: "30 jour"
+      duration: "30 jour",
     },
     {
       id: "shahid1000",
       name: "SHAHID 1000",
-      data: "06 Go internet",
-      features: [
-        "Accès shahid mobile",
-        "06 Go internet"
-      ],
+      data: "6Go internet",
+      features: ["Accès shahid mobile", "6Go internet"],
       price: "1000",
-      duration: "30 jour"
-    }
+      duration: "30 jour",
+    },
   ],
   ar: [
     {
       id: "shahid500",
       name: "شاهد 500",
-      data: "02 Go إنترنت",
-      features: [
-        "الوصول إلى شاهد موبايل",
-        "02 Go إنترنت"
-      ],
+      data: "2Go إنترنت",
+      features: ["الوصول إلى شاهد موبايل", "2Go إنترنت"],
       price: "500",
-      duration: "30 يوم"
+      duration: "30 يوم",
     },
     {
       id: "shahid1000",
       name: "شاهد 1000",
-      data: "06 Go إنترنت",
-      features: [
-        "الوصول إلى شاهد موبايل",
-        "06 Go إنترنت"
-      ],
+      data: "6Go إنترنت",
+      features: ["الوصول إلى شاهد موبايل", "6Go إنترنت"],
       price: "1000",
-      duration: "30 يوم"
-    }
-  ]
+      duration: "30 يوم",
+    },
+  ],
 };
 
 export const shahidDescription = {
   fr: "Obtenez dès maintenant un forfait Shahid. 30 jours d'accès premium Shahid + Internet",
-  ar: "احصل على صيانة Shahid مجانًا، 30 يومًا من الوصول إلى Shahid المميز + الإنترنت"
+  ar: "احصل على صيانة Shahid مجانًا، 30 يومًا من الوصول إلى Shahid المميز + الإنترنت",
 };
