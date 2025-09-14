96a3f16e0bab841f43fb1de5a017936c4361e3ed
Abderrezague ismail <ki_abderrezague@esi.dz>
2025-09-14 15:53:40 +0100

    setup data pro

---
+++ data-pro/public/components/Forfait/ModalData.js
@@ -0,0 +1,52 @@
+export default {
+  fr: {
+    "Forfait 100": {
+      confirm: `700Mo + YouTube illimité, valables 24H pour 100 DA seulement`,
+      success: `700 Mo + YouTube illimité attribués, valables 24H.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 100. Veuillez recharger votre compte.`,
+    },
+    "Forfait 300": {
+      confirm: `3Go + YouTube illimité, valables 72H pour 300 DA seulement`,
+      success: `3Go + YouTube illimité attribués, valables 72H`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 300. Veuillez recharger votre compte.`,
+    },
+    "Forfait 1000": {
+      confirm: `15Go + 5Go YouTube valables jusqu'à la fin de votre cycle de facturation, pour 1000 DA seulement`,
+      success: `15Go + 5Go YouTube attribués, valables jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 1000. Veuillez recharger votre compte.`,
+    },
+    "Forfait 1500": {
+      confirm: `40Go + YouTube illimité, valables jusqu'à la fin de votre cycle de facturation, pour 1500 DA seulement`,
+      success: `40Go + YouTube illimité attribués, valables jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 1500. Veuillez recharger votre compte.`,
+    },
+  },
+
+  ar: {
+    "اشتراك 100": {
+      confirm: `700Mo + يوتيوب غير محدود، صالحين 24 ساعة بـ 100 دج فقط`,
+      success: `لقد حصلت على 700Mo + يوتيوب غير محدود، صالحين 24 ساعة.`,
+      insufficient: `رصيدك غير كافٍٍ لشراء الاشتراك 100. يُرجى تعبئة رصيدك.`,
+    },
+    "اشتراك 300": {
+      confirm: `3Go + يوتيوب غير محدود، صالحين 72 ساعة بـ 300 دج فقط`,
+      success: `لقد حصلت على 3Go + يوتيوب غير محدود، صالحين 72 ساعة.`,
+      insufficient: `رصيدك غير كافٍٍ لشراء الاشتراك 300. يُرجى تعبئة رصيدك.`,
+    },
+    "Smart 50": {
+      confirm: `200Mo إنترنت صالحين 24 ساعة بـ 50 دج`,
+      success: `لقد حصلت على 200Mo إنترنت صالحين 24 ساعة.`,
+      insufficient: `رصيدك غير كافٍٍ لشراء الاشتراك Smart 50. يُرجى تعبئة رصيدك.`,
+    },
+    "اشتراك 1000": {
+      confirm: `5Go + 15Go يوتيوب صالحين إلى غاية نهاية دورة الفوترة الخاصة بك، بـ 1000 دج فقط`,
+      success: `لقد حصلت على 5Go + 15Go يوتيوب، صالحين إلى غاية نهاية دورة الفوترة الخاصة بك.`,
+      insufficient: `رصيدك غير كافٍٍ لشراء الاشتراك 1000. يُرجى تعبئة رصيدك.`,
+    },
+    "اشتراك 1500": {
+      confirm: `40Go + يوتيوب غير محدود، صالحين إلى غاية نهاية دورة الفوترة الخاصة بك، بـ 1500 دج فقط`,
+      success: `لقد حصلت على 40Go + يوتيوب غير محدود، صالحين إلى غاية نهاية دورة الفوترة الخاصة بك.`,
+      insufficient: `رصيدك غير كافٍٍ لشراء الاشتراك 1500. يُرجى تعبئة رصيدك.`,
+    },
+  },
+};
