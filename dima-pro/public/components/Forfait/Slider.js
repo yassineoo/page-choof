4bc83f48ae183475bac76793f5ef489d4e684ebf
Abderrezague ismail <ki_abderrezague@esi.dz>
2025-09-16 12:49:07 +0100

    fixing dima pro mixed fonts

--- dima-pro/public/components/Forfait/Slider.js
+++ dima-pro/public/components/Forfait/Slider.js
@@ -68,7 +68,13 @@ export class Slider {
         <div class="p-6 forfait-card-container h-full" ${isRTL ? `dir="rtl"` : ``}>
           <div class="pb-4">
             <h2 class="${titleFontClass} font-medium text-2xl text-center capitalize text-black dark:text-white mb-4 leading-tight">
-                 ${offer.name}
+                 ${isRTL && index < 4
+                  ? 
+                  `<span>
+                    اشتراك <span class="font-rubik">${offer.price}</span>
+                  </span>`
+                  : 
+                  offer.name}
             </h2>
             <div class="w-full h-px forfait-divider mb-4"></div>
           </div>
