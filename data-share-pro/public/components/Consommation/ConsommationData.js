915924de462e3d3ce5bf047d119388e8b8f934f0
yassineoo <jy_attou@esi.dz>
2025-09-16 12:11:29 +0100

    add gold

---
+++ data-share-pro/public/components/Consommation/ConsommationData.js
@@ -0,0 +1,120 @@
+export const consommationData = {
+  fr: {
+    title: "Mon compte",
+    charge: "Recharger mon compte",
+    cards: [
+      {
+        icon: "rocket",
+        title: "Crédit",
+        sections: [
+          {
+            subIcon: "dollar-phone",
+            subtitle: "Crédit recharge",
+            value: "4000",
+            date: "26/05/25",
+            unit: "DA",
+          },
+        ],
+      },
+      {
+        icon: "internet",
+        title: "Internet",
+        sections: [
+          {
+            subIcon: "internet",
+            subtitle: "Internet offert",
+            value: "1",
+            unit: "To",
+            date: "26/05/25",
+            percentage: 50,
+            extra: "/2To",
+          },
+          {
+            subIcon: "internet",
+            subtitle: "Internet mensuel",
+            value: "15",
+            unit: "Go",
+            date: "26/05/25",
+            percentage: 100,
+            extra: "/15Go",
+          },
+          {
+            subIcon: "internet",
+            subtitle: "Consommation en Mo",
+            value: "700",
+            unit: "Mo",
+            date: "26/05/25",
+          },
+          {
+            subIcon: "internet",
+            subtitle: "Seuil de consommation total",
+            value: "80",
+            unit: "Go",
+            date: "26/05/25",
+          },
+        ],
+      },
+    ],
+  },
+  ar: {
+    title: "حسابي",
+    charge: "تعبئة رصيدي",
+    cards: [
+      {
+        icon: "rocket",
+        title: "الرصيد",
+        sections: [
+          {
+            subIcon: "dollar-phone",
+            subtitle: "رصيد التعبئة",
+            value: '<span class="font-rubik">4000</span>',
+            unit: "دج",
+            date: "26/05/25",
+          },
+        ],
+      },
+      {
+        icon: "internet",
+        title: "الإنترنت",
+        sections: [
+          {
+            subIcon: "internet",
+            subtitle: "إنترنت مُهدى",
+            value: "40",
+            unit: "Go",
+            date: "26/05/25",
+            percentage: 50,
+            extra: "/80Go",
+          },
+          {
+            subIcon: "internet",
+            subtitle: "إنترنت شهري",
+            value: "15",
+            unit: "Go",
+            date: "26/05/25",
+            percentage: 100,
+            extra: "/15Go",
+          },
+          {
+            subIcon: "internet",
+            subtitle: "إنترنت يومي",
+            value: "250",
+            unit: "Mo",
+            date: "26/05/25",
+            percentage: 50,
+            extra: "/500Mo",
+          },
+          {
+            subIcon: "internet",
+            subtitle: "إنترنت متبقي",
+            value: "30",
+            unit: "Go",
+            date: "26/05/25",
+            percentage: 50,
+            extra: "/60Go",
+          },
+        ],
+      },
+    ],
+  },
+};
---
+++ data-share-pro/public/components/Forfait/ModalData.js
@@ -0,0 +1,65 @@
+export default {
+  fr: {
+    "Forfait 50 000": {
+      confirm: `1To d'internet pour 50 000 DA`,
+      success: `Forfait internet 1To attribué, valable jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 50 000. Veuillez recharger votre compte.`,
+    },
+
+    "Forfait 30 000": {
+      confirm: `500Go d'internet pour 30 000 DA`,
+      success: `Forfait internet 500Go attribué, valable jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 30 000. Veuillez recharger votre compte.`,
+    },
+
+    "Forfait 2500": {
+      confirm: `100Go d'internet pour 2500 DA`,
+      success: `Forfait internet 100Go attribué, valable jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 2500. Veuillez recharger votre compte.`,
+    },
+
+    "Forfait 1200": {
+      confirm: `40Go d'internet pour 1200 DA`,
+      success: `Forfait internet 40Go attribué, valable jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 1200. Veuillez recharger votre compte.`,
+    },
+
+    "Forfait 500": {
+      confirm: `12Go d'internet pour 500 DA`,
+      success: `Forfait internet 12Go attribué, valable jusqu'à la fin de votre cycle de facturation.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 500. Veuillez recharger votre compte.`,
+    },
+  },
+
+  ar: {
+    "اشتراك 50 000": {
+      confirm: `1To حجم إنترنت بـ 000 50 دج`,
+      success: `لديك 1To حجم إنترنت، صالح إلى غاية نهاية دورة فوترتك`,
+      insufficient: `رصيدك غير كافٍ لشراء الاشتراك 000 50. يُرجى تعبئة رصيدك.`,
+    },
+
+    "اشتراك 30 000": {
+      confirm: `500Go حجم إنترنت بـ 000 30 دج`,
+      success: `لديك 500Go حجم إنترنت، صالح إلى غاية نهاية دورة فوترتك`,
+      insufficient: `رصيدك غير كافٍ لشراء الاشتراك 000 30. يُرجى تعبئة رصيدك.`,
+    },
+
+    "اشتراك 2500": {
+      confirm: `100Go حجم إنترنت بـ 2500 دج`,
+      success: `لديك 100Go حجم إنترنت، صالح إلى غاية نهاية دورة فوترتك`,
+      insufficient: `رصيدك غير كافٍ لشراء الاشتراك 2500. يُرجى تعبئة رصيدك.`,
+    },
+
+    "اشتراك 1200": {
+      confirm: `40Go حجم إنترنت بـ 1200 دج`,
+      success: `لديك 40Go حجم إنترنت، صالح إلى غاية نهاية دورة فوترتك`,
+      insufficient: `رصيدك غير كافٍ لشراء الاشتراك 1200. يُرجى تعبئة رصيدك.`,
+    },
+
+    "اشتراك 500": {
+      confirm: `12Go حجم إنترنت بـ 500 دج`,
+      success: `لديك 12Go حجم إنترنت، صالح إلى غاية نهاية دورة فوترتك`,
+      insufficient: `رصيدك غير كافٍ لشراء الاشتراك 500. يُرجى تعبئة رصيدك.`,
+    },
+  },
+};
