1f909ef20d53e6c8b2b6bff2b28264a4ee06a73d
Akkal Abdelbaki Nazim <fleurvertesurfondblanc@gmail.com>
2025-09-16 19:46:40 +0200

    Creation of page SHIFT

---
+++ SHIFT/public/components/Forfait/ModalData.js
@@ -0,0 +1,63 @@
+export default {
+  fr: {
+    "Smart 200": {
+      confirm: `INTERNET illimité, valable 24H pour 200 DA seulement.`,
+      success: `INTERNET illimité attribué, valable 24H.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait Smart 200. Veuillez recharger votre compte.`,
+    },
+    "Smart 100": {
+      confirm: `100 DA vers TOUS les réseaux + Facebook et Messenger illimités + 500Mo, valables 24H pour 100 DA seulement`,
+      success: `100 DA vers TOUS les réseaux + Facebook et Messenger illimités + 500Mo, valables 24H attribués.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait Smart 100. Veuillez recharger votre compte.`,
+    },
+    "Forfait 100": {
+      confirm: `700Mo + YouTube illimité, valables 24H pour 100 DA seulement.`,
+      success: `700Mo + YouTube illimité attribués, valables 24H.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 100. Veuillez recharger votre compte.`,
+    },
+    "Forfait 300": {
+      confirm: `3Go + YouTube illimité, valables 72H pour 300 DA seulement.`,
+      success: `3Go + YouTube illimité attribués, valables 72H.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 300. Veuillez recharger votre compte.`,
+    },
+    "Forfait 1000": {
+      confirm: `15Go + 5Go YouTube valables 30 jours, pour 1000 DA seulement.`,
+      success: `15Go + 5Go YouTube attribués, valables 30 jours.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 1000. Veuillez recharger votre compte.`,
+    },
+    "Forfait 1500": {
+      confirm: `40Go + YouTube illimité valables 30 jours, pour 1500 DA seulement.`,
+      success: `40Go + YouTube illimité attribués, valables 30 jours.`,
+      insufficient: `Votre crédit est insuffisant pour acheter le forfait 1500. Veuillez recharger votre compte.`,
+    },
+  },
+
+  ar: {
+  "Smart 200": {
+    confirm: `إنترنت غير محدودة صالحة لمدة 24 ساعة بـ 200 دج فقط.`,
+    success: `لقد حصلت على إنترنت غير محدودة صالحة لمدة 24 ساعة`,
+    insufficient: `رصيدك غير كافٍ لشراء الاشتراك Smart 200. يُرجى تعبئة رصيدك.`,
+  },
+  "اشتراك 100": {
+    confirm: `700Mo + يوتيوب غير محدود، صالحين لمدة 24 ساعة بـ 100 دج فقط.`,
+    success: `لقد حصلت على 700Mo + يوتيوب غير محدود، صالحين لمدة 24 ساعة`,
+    insufficient: `رصيدك غير كافٍ لشراء الاشتراك 100. يُرجى تعبئة رصيدك.`,
+  },
+  "اشتراك 300": {
+    confirm: `3Go + يوتيوب غير محدود، صالحين لمدة 72 ساعة بـ 300 دج فقط.`,
+    success: `لقد حصلت على 3Go + يوتيوب غير محدود، صالحين لمدة 72 ساعة`,
+    insufficient: `رصيدك غير كافٍ لشراء الاشتراك 300. يُرجى تعبئة رصيدك.`,
+  },
+  "اشتراك 1000": {
+    confirm: `5Go + 15Go يوتيوب صالحين لمدة 30 يوم، بـ 1000 دج فقط`,
+    success: `لقد حصلت على 5Go + 15Go يوتيوب صالحين لمدة 30 يوم`,
+    insufficient: `رصيدك غير كافٍ لشراء الاشتراك 1000. يُرجى تعبئة رصيدك.`,
+  },
+  "اشتراك 1500": {
+    confirm: `40Go + يوتيوب غير محدود، صالحين لمدة 30 يوم، بـ 1500 دج فقط`,
+    success: `لقد حصلت على 40Go + يوتيوب غير محدود، صالحين لمدة 30 يوم`,
+    insufficient: `رصيدك غير كافٍ لشراء الاشتراك 1500. يُرجى تعبئة رصيدك.`,
+  },
+},
+
+};
