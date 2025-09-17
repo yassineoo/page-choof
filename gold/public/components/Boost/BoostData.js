6485fb8293617de0000259fffac924a43275768b
Abderrezague ismail <ki_abderrezague@esi.dz>
2025-09-17 16:06:35 +0100

    fixing data share pro dashboard

---
+++ gold/public/components/Boost/BoostData.js
@@ -0,0 +1,38 @@
+// BoostData.js
+export const boostData = {
+  fr: {
+    title: "BOOST 500",
+    description: "Booster votre forfait Gold 2000 et bénéficier de 30Go + appels illimités vers tous les réseaux nationaux + SMS illimités vers Ooredoo + 100 SMS vers les autres réseaux nationaux, le tout selon la validité de votre forfait Gold 2000 en cours.",
+    confirmDescription:
+      "Pour 500da seulement, vous pouvez booster votre forfait Dima 2000 et bénéficiez d'appels illimités vers tous les réseaux + 50Go et 50 SMS en plus, le tout selon la validité de votre forfait Dima 2000 en cours.",
+    price: "500 DA",
+    buy: "ACHETER",
+    confirmTitle: "BOOST 500",
+    confirmBtn: "CONFIRMER",
+    cancelBtn: "ANNULER",
+    successTitle: "FÉLICITATIONS !",
+    successMessage:
+      "Vous avez reçu des appels illimités vers tous les réseaux + 50Go + 50 SMS vers tous les réseaux nationaux, le tout selon la validité de votre forfait Dima 2000.",
+    insufficientTitle: "CRÉDIT INSUFFISANT",
+    insufficientMessage: "Votre crédit est insuffisant pour acheter le Boost 500. Veuillez recharger votre compte.",
+    ok: "OK",
+  },
+  ar: {
+    title: "BOOST 500",
+    description: "5 Go إنترنت صالحة حتى نهاية دورة الفوترة الخاصة بك",
+    confirmDescription:
+      "بـ 500 دج فقط، يمكنك تفعيل Boost على باقة Dima 2000: مكالمات غير محدودة نحو كل الشبكات + 50Go و 50 رسالة نصية إضافية، حسب صلاحية باقتك الحالية.",
+    price: "500 دج",
+    buy: "شراء",
+    confirmTitle: "BOOST 500",
+    confirmBtn: "تأكيد",
+    cancelBtn: "إلغاء",
+    successTitle: "مبروك !",
+    successMessage: "تمت إضافـة مكالمات غير محدودة نحو كل الشبكات + 50Go + 50 رسالة نصية وطنية، حسب صلاحية باقتك Dima 2000.",
+    insufficientTitle: "رصيد غير كافٍ",
+    insufficientMessage: "رصيدك غير كافٍ لشراء Boost 500. يرجى شحن رصيدك.",
+    ok: "تم",
+  },
+};
+
+export default boostData;
