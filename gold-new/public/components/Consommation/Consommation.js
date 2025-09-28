d22e430c7122353fe281e2ee7cb906a791368857
Abderrezague ismail <ki_abderrezague@esi.dz>
2025-09-28 10:16:51 +0100

    fixing gold jdida issues

--- gold-new/public/components/Consommation/Consommation.js
+++ gold-new/public/components/Consommation/Consommation.js
@@ -389,7 +389,8 @@ export default class Consommation {
             ${data.title}
           </h1>
         </div>
-        <div class="w-full px-4 -mt-7 bg-[#F8F8F8]">
+        <div class="relative w-full px-4 -mt-7 bg-[#F8F8F8]">
+          <div class="absolute top-0 left-0 w-full h-[100px] bg-ooredoo-red -mt-1"></div>
           <div class="cards-container flex flex-col gap-6 max-w-full mx-auto">
             ${this.renderMobileCard(data.cards[0], 0, lang, theme, true, this.isCardExpanded(0))}
             ${
